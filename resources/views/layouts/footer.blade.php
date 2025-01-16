966b080a1311c3611fc4fc69d3dbfb51a8ae3c2d
mm <m@example.com>
2025-01-17 00:55:25 +0200

    first commit

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,7 @@
+<!-- Footer opened -->
+	<div class="main-footer ht-40">
+		<div class="container-fluid pd-t-0-f ht-100p">
+			<span>Copyright © Developed by <a href="https://www.linkedin.com/in/mohamed-nasser-0b1388239/">Mohamed Nasser</a> All rights reserved</span>
+		</div>
+	</div>
+<!-- Footer closed -->
